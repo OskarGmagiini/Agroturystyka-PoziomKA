269b66b6b327dce41a6964067921bdae3a964299
OskarGmagiini <gajdaoskar8@gmail.com>
2026-03-06 11:05:08 +0100

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,20 +1,21 @@
-const burger=document.querySelector(".burger-nav");
-const sideMenu=document.querySelector(".sideMenu");
-const sideMenuLinks=document.querySelectorAll(".sideMenuLinks")
-console.log(sideMenu);
-console.log(burger);
-var show = false;
+const burger = document.querySelector(".burger-nav");
+const sideMenu = document.querySelector(".sideMenu");
+const sideMenuLinks = document.querySelectorAll(".sideMenuLink");
 
-burger.addEventListener("click",function(){
-    console.log("Burger has been clicked")
-    sideMenu.classList.toggle("sideMenuShow")
-})
-sideMenuLinks.forEach(ml => {
-    console.log("askdjlkasdj")
-  ml.addEventListener("click", () => {
-    sideMenu.classList.toggle("sideMenuShow");
-    console.log("lkasclsk")
-    
-    
+burger.addEventListener("click", function() {
+    sideMenu.classList.toggle("active");
+});
 
-    })})
+sideMenuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        sideMenu.classList.remove("active");
+    });
+});
+
+// Close menu when clicking outside
+document.addEventListener("click", (e) => {
+    if (!sideMenu.contains(e.target) && !burger.contains(e.target)) {
+        sideMenu.classList.remove("active");
+    }
+});
+
